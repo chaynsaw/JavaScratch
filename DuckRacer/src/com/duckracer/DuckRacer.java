67d38538b23447acae1f220d65ca73e12cdc91f1
Chaynor Hsiao <chaynsaw@gmail.com>
2021-03-20 16:16:39 -0700

    Change raceProgress from String[] to List<String>

--- DuckRacer/src/com/duckracer/DuckRacer.java
+++ DuckRacer/src/com/duckracer/DuckRacer.java
@@ -1,19 +1,26 @@
 package com.duckracer;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class DuckRacer {
-    String[] raceProgress = new String[50];
-    public DuckRacer() {
-        raceProgress[0] = ".";
-        for (int i = 1; i < 49; i++) {
-            raceProgress[i] = " ";
+    int raceLength;
+    List<String> raceProgress = new ArrayList<>();
+    public DuckRacer(int raceLength) {
+        raceProgress.add(".");
+        for (int i = 1; i < raceLength; i++) {
+            raceProgress.add(" ");
         }
-        raceProgress[49] = ".";
+        raceProgress.add(".");
     }
     public void begin() {
-        int i = 0;
-        System.out.println(String.join("", Arrays.asList(raceProgress)));
+        int i = 1;
+        while (i < 50) {
+            raceProgress.set(i, ".");
+            i++;
+        }
+        System.out.println(String.join("", raceProgress));
     }
 }
--- DuckRacer/src/com/duckracer/client/Main.java
+++ DuckRacer/src/com/duckracer/client/Main.java
@@ -4,7 +4,7 @@ import com.duckracer.DuckRacer;
 
 class Main {
     public static void main(String[] args) {
-        DuckRacer duckRacer = new DuckRacer();
+        DuckRacer duckRacer = new DuckRacer(50);
         duckRacer.begin();
     }
 
