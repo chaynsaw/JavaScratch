96fbbb1f9a369295cdc46a21cf3d6f314e28c236
Chaynor Hsiao <chaynsaw@gmail.com>
2021-03-19 21:14:03 -0700

    Add scaffolding for new module: DuckRacer

---
+++ DuckRacer/src/com/duckracer/DuckRacer.java
@@ -0,0 +1,7 @@
+package com.duckracer;
+
+public class DuckRacer {
+    public void begin() {
+
+    }
+}
---
+++ DuckRacer/src/com/duckracer/client/Main.java
@@ -0,0 +1,11 @@
+package com.duckracer.client;
+
+import com.duckracer.DuckRacer;
+
+class Main {
+    public static void main(String[] args) {
+        DuckRacer duckRacer = new DuckRacer();
+        duckRacer.begin();
+    }
+
+}
