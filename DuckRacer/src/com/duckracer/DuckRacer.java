48c4d4876fb3328fa39dc88b28fb6779d601daec
Chaynor Hsiao <chaynsaw@gmail.com>
2021-03-19 21:24:31 -0700

    Add throws InterruptedException and play around with Thread.sleep

--- DuckRacer/src/com/duckracer/DuckRacer.java
+++ DuckRacer/src/com/duckracer/DuckRacer.java
@@ -1,7 +1,15 @@
 package com.duckracer;
 
 public class DuckRacer {
-    public void begin() {
-
+    public void begin() throws InterruptedException {
+        int i = 0;
+        StringBuilder string = new StringBuilder();
+        while (i < 10) {
+            System.out.println("\f");
+            Thread.sleep(1000);
+            i++;
+            string.append("-");
+            System.out.println(string);
+        }
     }
 }
--- DuckRacer/src/com/duckracer/client/Main.java
+++ DuckRacer/src/com/duckracer/client/Main.java
@@ -3,7 +3,7 @@ package com.duckracer.client;
 import com.duckracer.DuckRacer;
 
 class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         DuckRacer duckRacer = new DuckRacer();
         duckRacer.begin();
     }
